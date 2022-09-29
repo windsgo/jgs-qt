1121f31f735b50de24dccc7a062693c1e1407dda
windsgo <18221102427@163.com>
2022-09-29 20:59:09 +0800

    add icon, enable high dpi

--- jgs.pro
+++ jgs.pro
@@ -55,5 +55,4 @@ qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
 
-DISTFILES += \
-    3rdParts/myjgs/src/CMakeLists.txt
+RC_ICONS = hello.ico
--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QCoreApplication>
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
